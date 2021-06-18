c948a2ceb575da0c2c6802fabef8e2953a0d442c
Abhishek Ghosh <ghosh.abhi91@gmail.com>
2021-06-18 18:26:00 +0530

    ➕ Adds Product access JPA classes.

---
+++ products-list/src/main/java/com/abhishek/ecommercebackend/products/ProductService.java
@@ -0,0 +1,27 @@
+package com.abhishek.ecommercebackend.products;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<Product> getAllProduct() {
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(products::add);
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public void addAllProducts(List<Product> products) {
+        productRepository.saveAll(products);
+    }
+}
