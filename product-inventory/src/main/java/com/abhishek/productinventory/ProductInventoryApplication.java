f2cfe6d89a20177454c7e960c330470348782a81
Abhishek Ghosh <ghosh.abhi91@gmail.com>
2021-06-20 11:26:05 +0530

    ✔ Adds global exception handling

--- product-inventory/src/main/java/com/abhishek/productinventory/ProductInventoryApplication.java
+++ product-inventory/src/main/java/com/abhishek/productinventory/ProductInventoryApplication.java
@@ -1,11 +1,21 @@
 package com.abhishek.productinventory;
 
+import com.abhishek.productinventory.exception.RestTemplateResponseErrorHandler;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ProductInventoryApplication {
 
+	@Bean
+	public RestTemplate getInstance(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.errorHandler(new RestTemplateResponseErrorHandler()).build();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProductInventoryApplication.class, args);
 	}
